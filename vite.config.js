fe47c3550754749e1a31fc3ae95bf095a7e13a97
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-06-18 11:51:45 +0530

    again solving conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-
-  plugins: [react(),tailwindcss()]
-
-=======
-<<<<<<< HEAD
-  plugins: [react(),tailwindcss()],
-=======
-  plugins: [react(),   tailwindcss(),],
->>>>>>> 1e33c9e04eed1cf5071d838ddf24ca8483ea1af9
->>>>>>> base
-})
+  plugins: [react(), tailwindcss()],
+});

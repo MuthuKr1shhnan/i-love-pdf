d7dcacc4080b957091f9d1a525f3c747c9144d44
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-06-18 11:50:26 +0530

    debugging git conflict

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-
-  plugins: [react(),tailwindcss()]
-
-=======
-<<<<<<< HEAD
-  plugins: [react(),tailwindcss()],
-=======
-  plugins: [react(),   tailwindcss(),],
->>>>>>> 1e33c9e04eed1cf5071d838ddf24ca8483ea1af9
->>>>>>> base
-})
+  plugins: [react(), tailwindcss()],
+});
